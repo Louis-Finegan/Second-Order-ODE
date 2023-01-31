19061a56381c9bf200bc9d097a86753a814ab657
Louis-Finegan <louis02finegan@gmail.com>
2023-01-31 16:07:54 +0000

    Changes: pre publish

--- ode.py
+++ ode.py
@@ -2,7 +2,7 @@
 # Used libaries.
 import numpy as np
 from scipy.integrate import solve_ivp
-
+from scipy.optimize import fsolve
 
 class second_order_ode:
     def __init__(self, a: any, b: any, c: any, y0: list) -> None:
@@ -30,3 +30,61 @@ class second_order_ode:
 
         return solution.y[0], solution.y[1]
 
+    def eigen_values(self) -> tuple:
+        if self.a**2 - 4*self.b < 0:
+            m_1 = complex(-self.a/2, + np.sqrt(- self.a**2 + 4*self.b)/2)
+            m_2 = complex(-self.a/2, - np.sqrt(- self.a**2 + 4*self.b)/2)
+
+        elif self.a**2 - 4*self.b == 0:
+            m_1 = -self.a/2
+            m_2 = -self.a/2
+
+        else:
+            m_1 = -self.a/2 + np.sqrt(self.a**2 - 4*self.b)/2
+            m_2 = -self.a/2 - np.sqrt(self.a**2 - 4*self.b)/2
+        
+        return (m_1, m_2)
+
+    
+    def equilibrium(self):
+        
+        def eq_equation(y):
+            return [y[1], - self.a*y[1] - self.b*y[0]]
+
+        equilibrium = fsolve(eq_equation, [0.0, 0.0])
+
+        return equilibrium
+
+class first_order_system:
+    
+    def __init__(self, equation_1: any, equation_2: any, y0: list) -> None:
+        self.n = 100
+        self.dt = 1/self.n
+        self.t_int = [0, 10]
+        self.equation_1 = equation_1
+        self.equation_2 = equation_2
+        self.y0 = y0
+
+    def solve_system(self):
+        def equation(t, y):
+            x, y = y
+            dydt = [self.equation_1(x, y), self.equation_2(x, y)]
+            return dydt
+
+        solution = solve_ivp(lambda t, y: equation(t, y), 
+                            self.t_int, 
+                            self.y0, 
+                            t_eval=np.arange(self.t_int[0], 
+                                            self.t_int[1], 
+                                            self.dt))
+
+        return solution.y[0], solution.y[1]
+
+    def equilibrium(self):
+        
+        def eq_equation(y):
+            return [self.equation_1(y[0], y[1]), self.equation_2(y[0], y[1])]
+
+        equilibrium = fsolve(eq_equation, [0.0, 0.0])
+
+        return equilibrium
